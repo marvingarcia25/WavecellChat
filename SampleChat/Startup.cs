75292b50b13a7c3e43d61318ae3652260285b7b5
Marvin Garcia <marvin_garcia_25@yahoo.co>
2016-05-07 21:02:15 +0800

    Initial Commit

---
+++ SampleChat/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SampleChat.Startup))]
+
+namespace SampleChat
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
